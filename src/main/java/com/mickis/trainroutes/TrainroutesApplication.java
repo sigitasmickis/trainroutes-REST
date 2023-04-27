3e42051e86549a8f55cb786e204484121a6bd094
Sigitas Mickis <37709899+MiKiEs69@users.noreply.github.com>
2023-04-27 15:31:02 +0300

    Init commit

---
+++ src/main/java/com/mickis/trainroutes/TrainroutesApplication.java
@@ -0,0 +1,13 @@
+package com.mickis.trainroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainroutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mickis/trainroutes/TrainroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mickis.trainroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
